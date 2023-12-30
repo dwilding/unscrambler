59298164ebd6a965c4965ff16bae45b858377ed9
Dave Wilding <tech@dpw.me>
2023-12-30 08:40:04 +0000

    Prototype of fully hosted version

--- app/post-query.php
+++ app/post-query.php
@@ -19,51 +19,43 @@ if ($request_body === null) {
   respond_with_failure();
 }
 
-// Get DeepL key
-if (!array_key_exists('key', $request_body)) {
+// Get query
+if (!array_key_exists('query', $request_body)) {
   respond_with_failure();
 }
-$key = $request_body['key'];
+$query = $request_body['query'];
 
-// Get DeepL endpoint
-if (!array_key_exists('endpoint', $request_body)) {
-  respond_with_failure();
-}
-$endpoint = $request_body['endpoint'];
-if ($endpoint != 'api.deepl.com' && $endpoint != 'api-free.deepl.com') {
-  respond_with_failure();
-}
+// Load secrets
+$secrets = json_decode(file_get_contents($_SERVER['APP_DIR_DATA'] . '/secrets.json'), true);
 
-// Get English text
-if (!array_key_exists('text', $request_body)) {
-  respond_with_failure();
-}
-$text = $request_body['text'];
+// Generate English text
+$english = 'I am a banana'; // TODO implement this properly
 
 // Translate English text
 $deepl_request_data = [
   'source_lang' => 'EN',
   'target_lang' => 'ZH',
   'text' => [
-    $text
+    $english
   ]
 ];
-$deepl_request = curl_init('https://' . $endpoint . '/v2/translate');
+$deepl_request = curl_init('https://api-free.deepl.com/v2/translate');
 curl_setopt($deepl_request, CURLOPT_POST, 1);
 curl_setopt($deepl_request, CURLOPT_POSTFIELDS, json_encode($deepl_request_data));
 curl_setopt($deepl_request, CURLOPT_HTTPHEADER, [
   'Content-Type: application/json',
-  'Authorization: DeepL-Auth-Key ' . $key
+  'Authorization: DeepL-Auth-Key ' . $secrets['keyDeepL']
 ]);
 curl_setopt($deepl_request, CURLOPT_RETURNTRANSFER, true);
 $deepl_response = curl_exec($deepl_request);
 curl_close($deepl_request);
 $deepl_result = json_decode($deepl_response, true);
 
-// Respond with translated text
+// Respond with English text and translated text
 echo json_encode([
   'success' => true,
-  'text' => $deepl_result['translations'][0]['text']
+  'english' => $english,
+  'translated' => $deepl_result['translations'][0]['text']
 ]);
 
 ?>
