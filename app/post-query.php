f537ce39bbc89261382136a109a73c672893a52f
Dave Wilding <tech@dpw.me>
2024-01-01 00:58:08 +0000

    Clean the pinyin in multiple steps

--- app/post-query.php
+++ app/post-query.php
@@ -28,23 +28,21 @@ if (!array_key_exists('query', $request_body)) {
 }
 $query = substr($request_body['query'], 0, 200);
 
-// Load secrets
+// Prepare to call APIs
 $secrets = json_decode(file_get_contents($_SERVER['APP_DIR_DATA'] . '/secrets.json'), true);
-
-// Generate English text
-function generate_english($query) {
+function call_gpt($temperature, $user, $system) {
   global $secrets;
   $request_data = [
     'model' => 'gpt-3.5-turbo-1106',
-    'temperature' => 0.3,
+    'temperature' => $temperature,
     'messages' => [
       [
         'role' => 'system',
-        'content' => 'You are a language assistant. The user will try to express something using a mix of Chinese and English. You must rephrase the user\'s text in simple English. Do not respond with anything else; no discussion is needed. Your response must be easily understood by non-native speakers of English, so please keep the vocab and grammar as simple as possible. If the user\'s text is already in simple English, you can return the text as is.'
+        'content' => $system
       ],
       [
         'role' => 'user',
-        'content' => $query
+        'content' => $user
       ]
     ]
   ];
@@ -61,16 +59,13 @@ function generate_english($query) {
   $result = json_decode($response, true);
   return $result['choices'][0]['message']['content'];
 }
-$english = substr(generate_english($query), 0, 500);
-
-// Translate English text
-function translate_english($english) {
+function call_deepl($text) {
   global $secrets;
   $request_data = [
     'source_lang' => 'EN',
     'target_lang' => 'ZH',
     'text' => [
-      $english
+      $text
     ]
   ];
   $request = curl_init('https://api-free.deepl.com/v2/translate');
@@ -86,48 +81,25 @@ function translate_english($english) {
   $result = json_decode($response, true);
   return $result['translations'][0]['text'];
 }
-$translated = translate_english($english);
+
+// Generate English text
+$english = call_gpt(0.3, $query, 'You are a language assistant. The user will try to express something using a mix of Chinese and English. You must rephrase the user\'s text in simple English. Do not respond with anything else; no discussion is needed. Your response must be easily understood by non-native speakers of English, so please keep the vocab and grammar as simple as possible. If the user\'s text is already in simple English, you can return the text as is.');
+$english = substr($english, 0, 500);
+
+// Translate English text
+$translated = call_deepl($english);
 
 // Convert translated text to pinyin
-function convert_to_pinyin($translated) {
-  global $secrets;
-  $pinyin = Pinyin::sentence($translated)->join(' ');
-  $request_data = [
-    'model' => 'gpt-3.5-turbo-1106',
-    'temperature' => 0.3,
-    'messages' => [
-      [
-        'role' => 'system',
-        'content' => 'You are a language assistant. The user will provide Chinese text followed by a pinyin transliteration. The transliteration will not account for polyphones, so some words might be inaccurate. You must respond with an accurate transliteration. Please also adjust the word and punctuation spacing to look more natural. Do not respond with anything else; no discussion is needed.'
-      ],
-      [
-        'role' => 'user',
-        'content' => $translated . "\n" . $pinyin
-      ]
-    ]
-  ];
-  $request = curl_init('https://api.openai.com/v1/chat/completions');
-  curl_setopt($request, CURLOPT_POST, 1);
-  curl_setopt($request, CURLOPT_POSTFIELDS, json_encode($request_data));
-  curl_setopt($request, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/json',
-    'Authorization: Bearer ' . $secrets['keyOpenAI']
-  ]);
-  curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-  $response = curl_exec($request);
-  curl_close($request);
-  $result = json_decode($response, true);
-  return $result['choices'][0]['message']['content'];
-}
-$pinyin = convert_to_pinyin($translated);
+$pinyin1 = Pinyin::sentence($translated)->join(' ');
+$pinyin2 = call_gpt(0.3, $translated . "\n" . $pinyin1, 'You are a language assistant. The user will provide Chinese text followed by a pinyin transliteration. The transliteration will not account for polyphones, so some words might be inaccurate. You must respond with an accurate transliteration. Do not respond with anything else; no discussion is needed.');
+$pinyin3 = call_gpt(0.3, $translated . "\n" . $pinyin2, 'You are a language assistant. The user will provide Chinese text followed by a pinyin transliteration. You must make the word spacing and punctuation spacing of the transliteration look as natural as possible. Respond with the updated transliteration only; no discussion is needed.');
 
 // Respond with English text and translated text
 echo json_encode([
   'success' => true,
   'english' => $english,
   'translated' => $translated,
-  'pinyin' => $pinyin,
-  'debug_pinyin' => Pinyin::sentence($translated)->join(' ')
+  'pinyin' => $pinyin3,
 ]);
 
 ?>
