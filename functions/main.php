23c4aafc86efc3a1dbd922830d244dc84feb30e6
Dave Wilding <tech@dpw.me>
2024-01-15 07:58:07 +0000

    Return pinyin in clauses

--- functions/main.php
+++ functions/main.php
@@ -8,7 +8,7 @@ function add_english(&$state, $secrets) {
   $gpt_data = call_gpt(
     $secrets,
     0.3,
-    FALSE,
+    false,
     $state['query'],
     'You are a language assistant. The user will try to express something using a mix of Chinese and English. You must rephrase the user\'s text in simple English. Do not respond with anything else; no discussion is needed. Your response must be easily understood by non-native speakers of English, so please keep the vocab and grammar as simple as possible. If the user\'s text is already in simple English, you can return the text as is.'
   );
@@ -23,33 +23,20 @@ function add_translated(&$state, $secrets) {
 }
 
 function add_pinyin(&$state, $secrets) {
+  $state['sequence'] = 3;
+  $state['pinyin'] = '<summary>Pinyin</summary>';
   $gpt_data = call_gpt(
     $secrets,
     0.3,
-    TRUE,
+    true,
     $state['translated'],
     'You are a language assistant. The user will provide Chinese text. You must split the text into individual clauses based on how Chinese is typically spoken. You must also remove any punctuation. Respond with a JSON object with a key called "clauses" that contains an array of the clauses.'
   );
-  $state['clauses'] = $gpt_data['output'];
-  $pinyin = Pinyin::sentence($state['translated'])->join(' ');
-  // // Pinyin might be innacurate because of polyphones. Use GPT to correct the pinyin
-  // $gpt_data = call_gpt(
-  //   $secrets,
-  //   0.3,
-  //   $state['translated'] . "\n" . $pinyin,
-  //   'You are a language assistant. The user will provide Chinese text on line 1 followed by a pinyin transliteration on line 2. The transliteration will not account for polyphones, so some words might be inaccurate. You must respond with an accurate transliteration. Do not respond with anything else; no discussion is needed.'
-  // );
-  // $pinyin = $gpt_data['output'];
-  // // Use GPT to format the pinyin (3.5 Turbo can't simultaneously correct and format pinyin)
-  // $gpt_data = call_gpt(
-  //   $secrets,
-  //   0.3,
-  //   $state['translated'] . "\n" . $pinyin, 
-  //   'You are a language assistant. The user will provide Chinese text on line 1 followed by a pinyin transliteration on line 2. You must make the word spacing and punctuation spacing of the transliteration look as natural as possible. Respond with the updated transliteration only; no discussion is needed.'
-  // );
-  // $pinyin = $gpt_data['output'];
-  $state['sequence'] = 3;
-  $state['pinyin'] = '<summary>Pinyin</summary><p>' . htmlspecialchars($pinyin) . '</p>';
+  $clauses = json_decode($gpt_data['output'], true)['clauses'];
+  foreach ($clauses as $clause) {
+    $pinyin = Pinyin::sentence($clause)->join(' ');
+    $state['pinyin'] .= '<p>' . htmlspecialchars($clause) . '<br>' . htmlspecialchars($pinyin) . '</p>';
+  }
 }
 
 ?>
