813692ac7be6be3210e5b75ac2d6e6b451bc58f1
Dave Wilding <tech@dpw.me>
2024-01-18 07:49:00 +0000

    Changed wording

--- app/index.php
+++ app/index.php
@@ -92,7 +92,7 @@ if (array_key_exists('q', $_GET)) {
           <a id="example" href="/?stream=no&q=我想%20stay%20两个%20weeks%20在中国">Get started with an example</a>
         </p>
         <p>
-          Unscrambler uses services from OpenAI and Microsoft to interpret your input.
+          Unscrambler interprets your input using OpenAI and Microsoft services.
           <a href="https://github.com/dwilding/unscrambler/#unscrambler--translate-a-mix-of-chinese-and-english" target="_blank">Learn more</a>
         </p>
       </div>
