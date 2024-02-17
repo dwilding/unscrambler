b1faa8984575e28c13407cfcabb26e3b5d141dee
Dave Wilding <tech@dpw.me>
2024-02-17 09:20:48 +0800

    Added help icon

--- functions/main.php
+++ functions/main.php
@@ -92,6 +92,8 @@ function perform_unscramble($secrets, &$state) {
   $translation = call_azure_translate($secrets, $english);
   $hanzi = $translation['text'];
   $state['outputHTML'] = '<p><em>' . htmlspecialchars($english) . '</em></p><p><mark>' . htmlspecialchars($hanzi) . '</mark></p>';
+  $help_msg = 'I\'m trying to say something like "' . $state['query'] . '" in Chinese. I asked a translation app for help and it told me to say "' . $hanzi . '". Please can you explain this translation in a bit more detail?';
+  $state['outputHTML'] .= '<p class="action"><a href="/copy?msg=' . rawurlencode($help_msg) . '" target="_blank" onclick="copy(event)">Copy AI help request</a> <a href="https://github.com/dwilding/unscrambler#how-it-works" target="_blank"><svg width="20px" height="20px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9 9C9 5.49997 14.5 5.5 14.5 9C14.5 11.5 12 10.9999 12 13.9999" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 18.01L12.01 17.9989" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></a></p>';
   $hanzi_sentences = split_sentences($hanzi, $translation['sentLen']['transSentLen']);
   $pinyin = $translation['transliteration']['text'];
   $pinyin_sentences = split_sentences($pinyin, call_azure_break_pinyin($secrets, $pinyin));
@@ -103,12 +105,10 @@ function perform_unscramble($secrets, &$state) {
   foreach ($sentences as $sentence) {
     $state['outputHTML'] .= '<p>' . htmlspecialchars($sentence['hanzi']) . '<br>' . htmlspecialchars($sentence['pinyin']) . '</p>';
   }
-  if ($state['query'] == '我想 stay 两个 weeks 在中国') {
-    $state['outputHTML'] .= '<p>You can also use pinyin in your input. <a href="/?stream=no&q=zhe%20ge%20city%20has%20a%20hen%20you%20yi%20si%20de%20history" onclick="demo(event)">Try another example</a></p>';
-  }
   $state['outputHTML'] .= '</details>';
-  $help_msg = 'I\'m trying to say something like "' . $state['query'] . '" in Chinese. I asked a translation app for help and it told me to say "' . $hanzi . '". Please can you explain this translation in a bit more detail?';
-  $state['outputHTML'] .= '<p class="action"><a href="/copy?msg=' . rawurlencode($help_msg) . '" target="_blank" onclick="copy(event)">Copy AI help request</a></p>';
+  if ($state['query'] == '我想 stay 两个 weeks 在中国') {
+    $state['outputHTML'] .= '<p><strong>Tip:</strong> You can also use pinyin in your input. <a href="/?stream=no&q=zhe%20ge%20city%20has%20a%20hen%20you%20yi%20si%20de%20history" onclick="demo(event)">Try another example</a></p>';
+  }
 }
 
 ?>
