ba5d6c80bf007dfa6072e4e739b304d2a16a87c8
Dave Wilding <tech@dpw.me>
2024-04-30 07:42:29 +0000

    Updated icons

--- app/index.php
+++ app/index.php
@@ -38,8 +38,9 @@ if (array_key_exists('q', $_GET)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="An experimental tool for people who want to improve their Chinese skills">
     <title>Unscrambler</title>
-    <link rel="icon" href="/icon.svg" type="image/svg+xml" id="icon">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="icon" href="/favicons/icon-16.png" sizes="16x16" type="image/png">
+    <link rel="icon" href="/favicons/icon-32.png" sizes="32x32" type="image/png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/icon-180-apple.png">
     <link rel="manifest" href="/manifest.webmanifest">
     <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
     <style>
